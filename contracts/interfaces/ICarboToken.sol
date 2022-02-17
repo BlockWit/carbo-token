c2252060724d3c1b2e800d1fcf4e69d33d0f504a
Arkadiy A.Davydov <jah.knows.me.well@gmail.com>
2022-02-17 17:31:07 +0300

    chore(contracts): add FeeManager

--- contracts/interfaces/ICarboToken.sol
+++ contracts/interfaces/ICarboToken.sol
@@ -7,7 +7,18 @@ pragma solidity ^0.8.0;
  */
 interface ICarboToken {
 
-    function getROwned(address account) external view returns(uint256);
-    function getRTotal() external view returns(uint256);
+    struct Fees {
+        uint256 rfi;
+        uint256 dividends;
+        uint256 buyback;
+        uint256 treasury;
+        uint256 liquidity;
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function getROwned(address account) external view returns (uint256);
+    function getRTotal() external view returns (uint256);
+    function getFees() external view returns (Fees memory, Fees memory);
 
 }
